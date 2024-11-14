546bc866a6ae059b4c98f900507c2c953ef69673
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-11-14 02:13:41 +0200

    Remove realease date from search list and show only realease year

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,14 +5,16 @@ export default function MovieCard({movieName, poster, date, commonGenres, movieG
     const mainURL = "https://image.tmdb.org/t/p/w500";
     const [genreName,setGenreName] = useState([])
 
-        useEffect(() => {
-            // Map genre IDs to names once when `commonGenres` or `movieGenres` change
-            const names = movieGenres.map(id => {
-                const genre = commonGenres.find(item => item.id === id);
-                return genre ? genre.name : null;
-            }).filter(Boolean); // filter out null values
-            setGenreName(names);
-        }, [commonGenres, movieGenres]);
+    useEffect(() => {
+        // Map genre IDs to names once when `commonGenres` or `movieGenres` change
+        const names = movieGenres.map(id => {
+            const genre = commonGenres.find(item => item.id === id);
+            return genre ? genre.name : null;
+        }).filter(Boolean); // filter out null values
+        setGenreName(names);
+    }, [commonGenres, movieGenres]);
+
+    const year = date ? date.split('-')[0] : 'N/A'; // Extract year from date
 
 
     return (
@@ -20,7 +22,7 @@ export default function MovieCard({movieName, poster, date, commonGenres, movieG
         <img src={poster == null ? posterPlaceholder : mainURL+poster}/>
         <div className='movieDetails'>
             <h3>{movieName}</h3>
-            {date}<br/>
+            {year}<br/>
             {genreName.join(", ")}                             
         </div>        
       </li>
