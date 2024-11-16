bf41e14044d4324646b64cf1825c87363f4de7b5
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-11-16 13:14:43 +0200

    Router handeling for homepage

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './bootstrap.min.css';
 import './index.css';
 import Home from './screens/Home';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+      <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
