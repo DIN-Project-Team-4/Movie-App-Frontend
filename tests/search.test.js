29c80b4368105487f2c831b33fb6eda56edb4e1e
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-11-28 01:07:18 +0200

    Check validity of year in advanced search. Update search tests

--- tests/search.test.js
+++ tests/search.test.js
@@ -1,20 +1,22 @@
 import {expect} from 'chai';
-import { searchByYear } from "../src/components/API_endpoints.js";
+import { searchByYear } from "../src/components/Search/searchApi.js";
 
-it('should return an error if searchText is not a valid number', async function () {
-    const result = await searchByYear('abc', 1);
+describe('Search by year',() => {
+    it('should return an error if searchText is not a valid number', async function () {
+        const result = await searchByYear('abc', 1);
 
-    expect(result).to.deep.equal({ error: 'Invalid year: must be a valid number' });
-});
+        expect(result).to.deep.equal({ error: 'Invalid year: must be a valid number' });
+    });
 
-it('should return an error if searchText is a float', async function () {
-    const result = await searchByYear('2022.5', 1);
+    it('should return an error if searchText is a float', async function () {
+        const result = await searchByYear('2022.5', 1);
 
-    expect(result).to.deep.equal({ error: 'Invalid year: must be a valid number' });
-});
+        expect(result).to.deep.equal({ error: 'Invalid year: must be a valid number' });
+    });
 
-it('should return an error if year is greater than current year', async function () {
-    const result = await searchByYear('2025', 1);
+    it('should return an error if year is greater than current year', async function () {
+        const result = await searchByYear('2025', 1);
 
-    expect(result).to.deep.equal({ error: 'Invalid year' });
-});
+        expect(result).to.deep.equal({ error: 'Invalid year' });
+    });
+})
