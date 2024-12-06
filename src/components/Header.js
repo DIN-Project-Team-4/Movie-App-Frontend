15a5e154cc9bdfec40c3d5ec487ef635de81c24a
Indunil Maheshika Liyanage <156415823+t3inli00@users.noreply.github.com>
2024-12-06 09:11:30 +0200

    Adjustment in logout area

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,12 +41,12 @@ const Header = ({ showSearchBox = true, showDropdownMenu = true }) => {
     localStorage.clear();
     setLoginstatus(true);
      // Delete all cookies
-    document.cookie.split(";").forEach(cookie => {
-    const accessToken = cookie.split("=");  
-    const refreshToken = cookie.split("=");  
-    document.cookie = `${accessToken}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    document.cookie = `${refreshToken}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-  }); 
+    // Delete all cookies
+  document.cookie.split(";").forEach(cookie => {
+    const tokens = cookie.split("=")[0].trim();
+    document.cookie = `${tokens}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;    
+   
+  });
       // Redirect to the home page
       navigate('/');
   };
