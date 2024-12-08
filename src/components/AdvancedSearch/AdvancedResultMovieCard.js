b6209a22c3f86b21a3a10474292e4916118b8e20
Upeksha Chiranthi <upekshachiranthi@gmail.com>
2024-12-08 18:07:33 +0200

    Link advanced search results to movie details page

--- src/components/AdvancedSearch/AdvancedResultMovieCard.js
+++ src/components/AdvancedSearch/AdvancedResultMovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import posterPlaceholder from '../../assets/image_unavailable.jpg';
 
 export default function AdvancedResultMovieCard({movie}) {
@@ -6,13 +7,17 @@ export default function AdvancedResultMovieCard({movie}) {
     // Extract year from date
     const year = movie.release_date ? movie.release_date.split('-')[0] : 'N/A'; 
     return (
-        <div className="movie-card">
-          <img src={movie.poster_path == null ? posterPlaceholder :mainURL + movie.poster_path} alt={movie.title} className="movie-image" />
-          <div className="movie-details">
-            <h8 className="movie-title-advanced-search">{movie.title}</h8>
-            <p className="movie-year">{year}</p>
-            <p className="movie-description">{movie.overview}</p>
+      <li>
+        <Link to={`/movie/${movie.id}`}>
+          <div className="movie-card">
+            <img src={movie.poster_path == null ? posterPlaceholder :mainURL + movie.poster_path} alt={movie.title} className="movie-image" />
+            <div className="movie-details">
+              <h8 className="movie-title-advanced-search">{movie.title}</h8>
+              <p className="movie-year">{year}</p>
+              <p className="movie-description">{movie.overview}</p>
+            </div>
           </div>
-        </div>
-      );
+        </Link>
+      </li>
+    );
 }
